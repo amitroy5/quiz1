706930386e4b4828ce4e9c811f9bcbab7b82cb2f
Amit Roy <amit5roy5@gmail.com>
2019-09-30 04:51:12 +0000

    Added model, database, and SimpeForm to quiz

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,21 @@
 class MainController < ApplicationController
   def index
+    @location = Location.last
   end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    Location.create(location_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:city, :state)
+  end
+
 end
